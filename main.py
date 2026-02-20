1c8583e7f1274243dc2e37e63c9320bddd29abca
Thomas <thomaelv@amazon.com>
2026-02-20 06:42:46 -0500

    using len in python

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello, VS Code")
+# This is a simple Python script that prompts the user for their name and then prints it out along with the length of the name.
+# name = input("What is your name? ")
+# print(name)
+# length = len(name)
+# print(length)
+
+# This line prompts the user for their name and then prints out the length of the name without storing it in a variable.
+# name = input("What is your name? ")
+# print(name)
+# length = len(name)
+# print(length)
+
+# print(len(input("what is your name? ")))

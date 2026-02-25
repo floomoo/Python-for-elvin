9087e33aa5aa8c84900080b88ff5aa60e2a1cdd1
floomoo <142675737+floomoo@users.noreply.github.com>
2026-02-25 08:53:31 -0500

    oparators

--- main.py
+++ main.py
@@ -55,3 +55,25 @@
 # # **
 # # * or /
 # # + or -
+
+# bmi = 84 / 1.65 ** 2
+
+
+# print(bmi)
+
+# #this will round up or down base on the number 
+# print(round(bmi))
+
+# #print how much space after the . you want the division to print
+# print(round(bmi, 2))
+
+# #User score a point
+# score = 0
+# score = score + 1
+# print(score)
+
+# #  +=,  -=   *=  /= all this can be used 
+# #easier way 
+# score = 2
+# score += 2
+# print(score)

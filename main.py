eee93ba93d39cccb6d550444e496cd4c7292f536
floomoo <142675737+floomoo@users.noreply.github.com>
2026-02-25 12:07:16 -0500

    tip calculator

--- main.py
+++ main.py
@@ -77,3 +77,40 @@
 # score = 2
 # score += 2
 # print(score)
+
+# score = 0
+# score += 0
+# print(score)
+
+# # f - string with out f string = "your score is " + str(score) if there are two string you will hve to use 
+# print("your score is " + str(score) )
+
+# age =  12
+# print(f"you are + {age} + ")
+# score = 0 
+# height = 1.8 
+# is_winning = True
+
+# print(f"Your score is = {score}, your height is {height}, and you are winning is {is_winning}")
+
+# name = input("What is your name? ")
+# print("Hello " + name )
+
+# age = 12
+# print("Your age is " + str(age) + " years old")
+
+# age =  12
+# print(f"you are + {age}  ")
+
+
+# tip calculator 
+# print("Welcome to the tip calculator!")
+# bill = float(input("What was the total bill? $"))
+# tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
+# people = int(input("How many people to split the bill? "))
+# tip_as_percent = tip / 100
+# total_tip_amount = bill * tip_as_percent        
+# total_bill = bill + total_tip_amount
+# bill_per_person = total_bill / people
+# final_amount = round(bill_per_person, 2)
+# print(f"Each person should pay: ${final_amount}")
--- task.py
+++ task.py
@@ -1,25 +1,8 @@
-bmi = 84 / 1.65 ** 2
 
 
-print(bmi)
+# score = 0
+# score += 0
+# print(score)
 
-#this will round up or down base on the number 
-print(round(bmi))
-
-#print how much space after the . you want the division to print
-print(round(bmi, 2))
-
-#User score a point
-score = 0
-score = score + 1
-print(score)
-
-#  +=,  -=   *=  /= all this can be used 
-#easier way 
-score = 2
-score += 2
-print(score)
-
-#f - string
-
-
+# # f - string with out f string = "your score is " + str(score) if there are two string you will hve to use 
+# print("your score is " + str(score) )
